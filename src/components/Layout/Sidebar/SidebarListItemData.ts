e814990631075fbe6b69e73e43fe6b69050593d9
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-08-30 19:32:10 +0600

    removed function to toggle between sidebar items

--- src/components/Layout/Sidebar/SidebarListItemData.ts
+++ src/components/Layout/Sidebar/SidebarListItemData.ts
@@ -5,36 +5,31 @@ interface ISidebarListItemData {
     title: string,
     icon: LucideIcon,
     path: string,
-    active: boolean,
     id: number 
 }
 
-export const SidebarListItemDataDefualt: ISidebarListItemData[] = [
+export const SidebarListItemData: ISidebarListItemData[] = [
     {
         title: 'Weather',
         icon: CloudSun,
-        active: true,
         path: '/Weather',
         id: 1
     },
     {
         title: 'Cities',
         icon: List,
-        active: false,
         path: '/Cities',
         id: 2
     },
     {
         title: 'Map',
         icon: Map,
-        active: false,
         path: '/Map',
         id: 3
     },
     {
         title: 'Settings',
         icon: Settings,
-        active: false,
         path: '/Settings',
         id: 4
     }
