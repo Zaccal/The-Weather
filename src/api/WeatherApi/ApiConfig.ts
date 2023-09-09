9545117949bd31a058afabcb3abf47243b3ab73f
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-09-09 22:10:40 +0600

    fix security

--- src/api/WeatherApi/ApiConfig.ts
+++ src/api/WeatherApi/ApiConfig.ts
@@ -1,7 +1,9 @@
+import { API_TOKEN } from '@/root'
+
 export const ApiConfig = {
   baseUrl: 'http://api.weatherapi.com/v1/',
   searchUrl: 'search.json',
   currentUrl: 'current.json',
   forecastUrl: 'forecast.json',
-  key: ''
+  key: API_TOKEN
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare global {
+  namespace NodeJs {
+    interface ProcessEnv {
+      API_TOKEN: string
+    }
+  }
+}
