969f2b30c374068a4c4a53d9f95485b171a828e8
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-09-09 19:54:07 +0600

    Edited styles

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,16 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-console': 'warn',
+    quotes: ['error', 'single'],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'comma-dangle': ['error', 'never']
+  }
 }
